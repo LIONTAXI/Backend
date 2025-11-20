575f6005695dccc393c9476a4fb3364543c9b0ce
yb2527 <yb2527@swu.ac.kr>
2025-11-21 01:28:46 +0900

    feat: 사용자 지도 위 마커 기능

--- src/main/java/taxi/tago/entity/User.java
+++ src/main/java/taxi/tago/entity/User.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +23,5 @@ public class User {
 
     private Double latitude; // 현재 위도
     private Double longitude; // 현재 경도
+    private LocalDateTime lastActiveAt; // 마지막으로 접속해서 활동한 시간
 }
