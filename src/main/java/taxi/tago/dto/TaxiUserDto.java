8306f0f77610cc8a808c4e9e1c36467c748d9639
yb2527 <yb2527@swu.ac.kr>
2025-12-01 00:15:57 +0900

    feat: 프사, 학번 필드 추가

--- src/main/java/taxi/tago/dto/TaxiUserDto.java
+++ src/main/java/taxi/tago/dto/TaxiUserDto.java
@@ -12,9 +12,9 @@ public class TaxiUserDto {
     public static class RequestResponse {
         private Long taxiUserId; // 신청 내역 ID
         private Long userId;     // 신청자 유저 ID
-        // private String userName; // TODO: 추후 수정(User 엔티티 확인)
-        // private Integer age;     // TODO: 추후 수정(User 엔티티 확인)
-        // TODO: 프로필 이미지 추가?
+        private String name;          // 이름
+        private String shortStudentId; // 학번 (2자리, 예: "23")
+        private String imgUrl;        // 프로필 사진 URL
         private ParticipationStatus status; // 요청 상태, WAITING 또는 ACCEPTED
     }
 }
