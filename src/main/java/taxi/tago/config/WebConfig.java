12898d997e674517b1e57b96ed2304f9a26bfbe3
yb2527 <yb2527@swu.ac.kr>
2025-12-09 14:17:40 +0900

    Revert "fix: 500 오류 수정 시도"

--- src/main/java/taxi/tago/config/WebConfig.java
+++ src/main/java/taxi/tago/config/WebConfig.java
@@ -1,7 +1,6 @@
 package taxi.tago.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,14 +14,5 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:///" + projectPath);
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")      // 모든 경로에 대해
-                .allowedOriginPatterns("*") // 모든 출처 허용
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 모든 HTTP 메서드 허용
-                .allowedHeaders("*")    // 모든 헤더 허용
-                .allowCredentials(true); // 쿠키/인증 정보 포함 허용
     }
 }
