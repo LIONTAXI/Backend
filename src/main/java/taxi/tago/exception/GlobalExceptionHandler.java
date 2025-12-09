92967b7c69b694796888d73177ac5ac812ed4dad
yb2527 <yb2527@swu.ac.kr>
2025-12-09 19:39:33 +0900

    fix: 예외처리 수정

--- src/main/java/taxi/tago/exception/GlobalExceptionHandler.java
+++ src/main/java/taxi/tago/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,6 +13,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST) // 400 Bad Request
-                .body(e.getMessage());
+                .body("[로직 에러] " + e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleJsonErrors(HttpMessageNotReadableException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("[데이터 형식 에러] 요청한 JSON 형식을 확인해주세요. (날짜/시간/타입 등)\n" + e.getMessage());
     }
 }
